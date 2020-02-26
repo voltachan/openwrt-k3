b6908f3f8923c4de0ac2de2f970f959bdec78538
伏特酱 <25198721+voltachan@users.noreply.github.com>
2020-02-26 14:57:57 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,4 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+git clone https://github.com/tzxiaozhen88/luci-app-koolproxyR ./openwrt/package/lean/luci-app-koolproxyR
+./scripts/feeds update -a
+./scripts/feeds install -a
